5c434c71e13d55557c65f85f2723adf095f922c5
akhil30802 <72270079+akhil30802@users.noreply.github.com>
2020-10-03 08:54:02 +0530

    Update Indian Kirana Store Calculator.py

--- Indian Kirana Store Calculator.py
+++ Indian Kirana Store Calculator.py
@@ -1,12 +1,60 @@
 # Python program to add a string of number which keeps adding numbers untill user press q key on keyboard
 sum = 0
 
-while (True):
-    userInput = (input("Enter the item price or press q to quit:  \n"))
-    if (userInput != 'q'):
-        sum = sum + int(userInput)
-        print(f"Order total so far: {sum}")
+ans = input(str("DO WANT TO PLAY THE GAME ?? \n"))
+
+while(True):
+
+
+    if ans == "yes" or ans == "Yes" or ans == "YES":
+
+
+        while (True):
+            userInput = (input("Enter the item price or press q to quit:  \n"))
+            if (userInput != 'q'):
+                sum = sum + int(userInput)
+                print(f"Order total so far: {sum}")
+            else:
+                print(f"Your bill total is {sum}")
+                print("Thanks for using calculator made by Gurpreet Singh Kochar")
+                break
+
+                
+# a more interactive calulator
+
+
+sum = 0
+
+ans = input(str('''DO WANT TO USE THE CALCULATOR?
+ Enter y for yes\n'''))
+
+while(True):
+
+
+    if ans == "y" or ans == "Y":
+
+
+        while (True):
+            userInput = (input("Enter the item price or press q to quit:  \n"))
+            if (userInput != 'q'):
+                sum = sum + int(userInput)
+                print(f"Order total so far: {sum}")
+            else:
+                print(f"Your bill total is {sum}")
+                print('''Thanks for using calculator
+Made by: Gurpreet Singh Kochar''')
+                break
+
     else:
-        print(f"Your bill total is {sum}")
-        print("Thanks for using calculator made by Gurpreet Singh Kochar")
-        break
+        print("""OK.
+COME WHEN WANT TO USE IT.""")
+
+
+    ans2=input(str('''DO WANT TO USE THE CALCULATOR AGAIN?
+ Enter y for yes\n'''))
+
+    if ans2 == "y":
+        continue
+    else:print("""Ok.
+VISIT OUR STORE AGAIN.""")
+    break
